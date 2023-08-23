4eef4f0ea4fcfcb5f17184f267d84ebf341e24de
Lukas B. Nordentoft <Lukasbnordentoft@gmail.com>
2023-08-23 15:36:26 +0200

    Updated setup.py with find_packages
    Changed packages variable in setup.py to use the find_packages() function, in order to correctly find the subpackages.

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(name='PyMGA',
       version='0.0.1',
@@ -7,7 +7,7 @@ setup(name='PyMGA',
       author='Tim Pedersen',
       author_email='timtoernes@gmail.com',
       license='MIT',
-      packages=['PyMGA'],
+      packages=find_packages(),
       install_requires=['numpy>=1.22',
                         'pypsa>=0.21',
                         'matplotlib',
